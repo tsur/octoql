7a5b57ec495bf72f88f4cdbf6e41f6a93e6dfef1
Tsur <prucheta@gmail.com>
2017-10-01 23:09:49 +0200

    fix(webpack): Update public path in production for gh-pages

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -12,7 +12,7 @@ module.exports = require('./webpack.base.babel')({
   output: {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
-    publicPath: '/octoql',
+    publicPath: '/octoql/',
   },
 
   plugins: [
